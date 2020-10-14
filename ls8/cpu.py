ee45880fa718ad350c3b0682dcc674d8db01b6e8
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-10-13 19:02:09 -0700

    Beautify the run() loop

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,10 +2,10 @@
 
 import sys
 
-HLT = 0b00000001
-LDI = 0b10000010
-PRN = 0b01000111
-MUL = 0b10100010
+HLT = 0b00000001  # 1
+LDI = 0b10000010  # 130
+PRN = 0b01000111  # 71
+MUL = 0b10100010  # 162
 
 
 class CPU:
@@ -16,6 +16,10 @@ class CPU:
         self.ram = [0] * 256
         self.register = [0] * 8
         self.program_counter = 0
+        self.dispatch_table = {}
+        self.dispatch_table[LDI] = self.handle_ldi
+        self.dispatch_table[PRN] = self.handle_prn
+        self.dispatch_table[MUL] = self.handle_mul
 
     def ram_read(self, memory_address_register):
         memory_data_register = self.ram[memory_address_register]
@@ -56,22 +60,6 @@ class CPU:
             print(f"File not found: {sys.argv[1]}")
             sys.exit(2)
 
-        # # For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010,  # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111,  # PRN R0
-        #     0b00000000,
-        #     0b00000001,  # HLT
-        # ]
-
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
-
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
@@ -103,6 +91,15 @@ class CPU:
 
         print()
 
+    def handle_ldi(self, a, b):
+        self.register[a] = b
+
+    def handle_prn(self, a):
+        print(self.register[a])
+
+    def handle_mul(self, op, a, b):
+        self.alu(op, a, b)
+
     def run(self):
         """Run the CPU."""
 
@@ -116,21 +113,22 @@ class CPU:
             # HLT (halt the CPU and exit the emulator)
             if instruction_register == HLT:
                 running = False
-                self.program_counter += 1
-            # LDI (set the value of a register to an integer)
-            elif instruction_register == LDI:
-                self.register[operand_a] = operand_b
-                self.program_counter += 3
-            # PRN (print numeric value stored in the given register)
-            elif instruction_register == PRN:
-                print(self.register[operand_a])
-                self.program_counter += 2
-            # MUL (multiply the values in two registers together and store the result in registerA)
-            elif instruction_register == MUL:
-                self.alu(MUL, operand_a, operand_b)
-                self.program_counter += 3
-            else:
-                print(f"Unkown instruction: {instruction_register}")
-                sys.exit(1)
 
-        print("Register: ", self.register)
+            # LDI (set the value of a register to an integer)
+            if instruction_register == LDI:
+                self.dispatch_table[LDI](operand_a, operand_b)
+
+            # PRN (print numeric value stored in the given register)
+            if instruction_register == PRN:
+                self.dispatch_table[PRN](operand_a)
+
+            # MUL (multiply the values in two registers together and store the result in registerA)
+            if instruction_register == MUL:
+                self.dispatch_table[MUL](MUL, operand_a, operand_b)
+
+            instruction_length = ((instruction_register & 0b11000000) >> 6) + 1
+            # this also works => instruction_length = (instruction_register >> 6) + 1
+            self.program_counter += instruction_length
+
+            # print(f"Unkown instruction: {instruction_register}")
+            # sys.exit(1)
